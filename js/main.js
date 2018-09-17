9c62626b75106129c6e9c69dd2e99fca51bfa5df
Tommy Danielsson <tdserviceco@gmail.com>
2018-09-17 10:14:31 +0200

    Added a calculation on my true age
    function for true age

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 const start = {
-  onReady: function(){
-    start.lazy_load()
+  onReady: function() {
+    start.lazy_load(), start.showYear();
   },
 
   lazy_load: function() {
-    jQuery('.presentation__content__intro').addClass('lazy-load');
+    jQuery(".presentation__content__intro").addClass("lazy-load");
+  },
+  showYear: function() {
+    var age = new Date().getFullYear() - 1986;
+    jQuery("#year").html(age);
   }
-}
+};
 
-jQuery( document ).ready( start.onReady );
+jQuery(document).ready(start.onReady);
